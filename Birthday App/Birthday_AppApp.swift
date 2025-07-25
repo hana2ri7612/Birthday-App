c4c3ce12d3de8466bd29386573f91435caa6883a
Hana Ri <hanacokeeri76@gmail.com>
2025-07-25 09:59:23 -0700

    Initial Commit

---
+++ Birthday App/Birthday_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Birthday_AppApp.swift
+//  Birthday App
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Birthday_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Birthday AppTests/Birthday_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  Birthday_AppTests.swift
+//  Birthday AppTests
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import Testing
+
+struct Birthday_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
