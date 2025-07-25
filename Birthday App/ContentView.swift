a3a2079ba82532fa66111868e1c10de366bcbce8
Hana Ri <hanacokeeri76@gmail.com>
2025-07-25 11:04:15 -0700

    Added basis of app, names, button, etc

--- Birthday App/ContentView.swift
+++ Birthday App/ContentView.swift
@@ -8,10 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [ Friend(name:"Emma", birthday: .now),
+        Friend(name:"Mia", birthday: Date(timeIntervalSince1970: 0))
+                                            
+                                            ]
+    @State private var newName=""
+    @State private var newBirthday = Date.now
     var body: some View {
-        
-    }
-}
+        NavigationStack{
+            List(friends, id: \.name) { friend in
+                HStack{
+                    Text (friend.name)
+                    Spacer()
+                    Text (friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationTitle("Birthdays!")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                        Text("New Birthday")
+                            .font(.headline)
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                                TextField("Name", text: $newName)
+                                    .textFieldStyle(.roundedBorder)
+                            }//Date
+                    
+                    Button("Save") {
+                        let newFriend = Friend (name:newName, birthday:newBirthday)
+                        friends.append(newFriend)
+                    }
+                    .padding(.all)
+                    }//Vstack
+            }//SafeArea
+           }//navigationstack
+    }//body
+}//Struct
 
 #Preview {
     ContentView()
---
+++ Friend.swift
@@ -0,0 +1,17 @@
+//
+//  Friend.swift
+//  Birthday App
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import Foundation
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date){
+        self.name = name
+        self.birthday = birthday
+    }
+}
